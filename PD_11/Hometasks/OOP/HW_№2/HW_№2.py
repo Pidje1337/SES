2d2c88bfa81e4a7b53f10ae873b39c7b1139ec62
Nick Kryutchkex <landuser2020@gmail.com>
2025-07-03 04:50:00 +0300

    Задание №5 hotfix: Сделал класс ColoredPoint наследником класса Point

--- PD_11/Hometasks/OOP/HW_№2/HW_№2.py
+++ PD_11/Hometasks/OOP/HW_№2/HW_№2.py
@@ -122,17 +122,14 @@ class Point:
 
 # Задание № 5
 
-class ColoredPoint:
+class ColoredPoint(Point):
 
     def __init__(self, x: int or float, y: int or float, color: str, standart_color: str = "Black"):
 
-        if not isinstance(x, int or float): raise TypeError("Ошибка: Недопустимые входные данные")
-        if not isinstance(y, int or float): raise TypeError("Ошибка: Недопустимые входные данные")
         if not isinstance(color, str): raise TypeError("Ошибка: Недопустимые входные данные")
         if not isinstance(standart_color, str): raise TypeError("Ошибка: Недопустимые входные данные")
 
-        self.x = x
-        self.y = y
+        super().__init__(x, y)
         self.color = color
         self.standart_color = standart_color
 
