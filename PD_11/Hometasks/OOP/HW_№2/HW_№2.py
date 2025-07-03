bb3e6eb61b07bdb29720b86c432c96ed3f8f8706
Nick Kryutchkex <landuser2020@gmail.com>
2025-07-03 03:06:42 +0300

    Созданы заготовки для классов

---
+++ PD_11/Hometasks/OOP/HW_№2/HW_№2.py
@@ -0,0 +1,45 @@
+# Задание № 1
+
+class Vector2D:
+
+    pass
+
+
+
+# Задание № 2
+
+class Money:
+
+    pass
+
+
+
+# Задание № 3
+
+class Time:
+
+    pass
+
+
+
+# Задание № 4
+
+class Point:
+
+    pass
+
+
+
+# Задание № 5
+
+class ColoredPoint:
+
+    pass
+
+
+
+# Задание № 6
+
+class Matrix:
+
+    pass
