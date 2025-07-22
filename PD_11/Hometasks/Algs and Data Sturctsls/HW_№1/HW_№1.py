82d2bb57cd36d3b79311fb614316dcbc0c6f92da
Nick Kryutchkex <landuser2020@gmail.com>
2025-07-23 02:46:15 +0300

    style(HW_№1): output messages in data validation blocks were translated into russian

--- PD_11/Hometasks/Algs and Data Sturctsls/HW_№1/HW_№1.py
+++ PD_11/Hometasks/Algs and Data Sturctsls/HW_№1/HW_№1.py
@@ -3,9 +3,9 @@
 def factorial(number: int) -> int:
 
     if not isinstance(number, int):
-        raise TypeError("Error: input must be an integer number")
+        raise TypeError("Ошибка: Входная переменная должна быть целым числом")
     if number < 0 or number > 20:
-        raise ValueError("Error: input integer must be bigger than 0 or less than 20")
+        raise ValueError("Ошибка: Вводимое число должно быть больше или равна 0 и меньше или равна 20")
 
     result = 1
     for counter in range(1, number):
@@ -20,7 +20,7 @@ def factorial(number: int) -> int:
 def count_ones(number: int) -> int:
 
     if not isinstance(number, int):
-        raise TypeError("Error: input must be an integer number")
+        raise TypeError("Ошибка: Входная переменная должна быть целым числом")
 
     counter = 0
     number = bin(number)[2::]
