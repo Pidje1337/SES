6350939cc1093f44c726cad3400ca466bc6ae6c2
Nick Kryutchkex <landuser2020@gmail.com>
2025-07-23 02:20:41 +0300

    feature(HW_№1:factorial): added input validation in factorial()

--- PD_11/Hometasks/Algs and Data Sturctsls/HW_№1/HW_№1.py
+++ PD_11/Hometasks/Algs and Data Sturctsls/HW_№1/HW_№1.py
@@ -2,6 +2,11 @@
 
 def factorial(number: int) -> int:
 
+    if not isinstance(number, int):
+        raise TypeError("Error: input must be an integer number")
+    if number < 0 or number > 20:
+        raise ValueError("Error: input integer must be bigger than 0 or less than 20")
+
     result = 1
     for counter in range(1, number):
         result *= counter
