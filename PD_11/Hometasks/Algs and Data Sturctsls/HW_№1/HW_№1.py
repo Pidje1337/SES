778427eaddccd1892f956615aeb6b2e2dc5d0af2
Nick Kryutchkex <landuser2020@gmail.com>
2025-07-28 03:10:21 +0300

    feat(HW_№1): created dates_attendance_stat function

--- PD_11/Hometasks/Algs and Data Sturctsls/HW_№1/HW_№1.py
+++ PD_11/Hometasks/Algs and Data Sturctsls/HW_№1/HW_№1.py
@@ -72,3 +72,44 @@ def is_palindrome(x: int) -> bool:
         x //= 10
         x %= 10^(num_of_digits - i)
     return True
+
+
+
+# Задание № 5
+
+def dates_attendance_stat(input_list: list, week_start_in_interval: int):
+
+    while len(input_list) % 7 != 0:
+        input_list.pop()
+
+    total_attendance_each_month = [0] * (len(input_list) // 28)
+    total_attendance_each_day = [0] * 7
+    week = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"]
+    month = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октрябрь", "Ноябрь", "Декабрь"]
+
+    for i in range(len(input_list)):
+        input_list[i][0] = input_list[i][0].split("-")
+        input_list[i] = [input_list[i][2], input_list[i][1], input_list[i][3]]
+        total_attendance_each_day[(i + week_start_in_interval) % 7] += input_list[i][3]
+        total_attendance_each_month[input_list[i][1]-1] += input_list[i][3]
+
+
+    most_attended_days = []
+    most_attended_months = []
+    least_attended_days = []
+
+    for i in range(len(total_attendance_each_day)):
+        if total_attendance_each_day[i] == max(total_attendance_each_day):
+            most_attended_days.append(f"{week[i]} - {total_attendance_each_day[i]} пользователей за весь указанный период")
+        if total_attendance_each_day[i] == min(total_attendance_each_day):
+            least_attended_days.append(f"{week[i]} - {total_attendance_each_day[i]} пользователей за весь указанный период")
+
+    for i in range(len(total_attendance_each_month)):
+        if total_attendance_each_month[i] == max(total_attendance_each_month):
+            most_attended_months.append(f"{month[i]} - {total_attendance_each_month[i]} пользователей за месяц")
+
+
+
+    return (f"Самые посещаемые дни:\n {most_attended_days}"
+            f"Самые малолюдные дни - {least_attended_days}"
+            f"Самый посещаемый месяц - {most_attended_months}")
