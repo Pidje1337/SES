6560b7305c3ed45556703e5c6fac6312d0f8e9ab
Nick Kryutchkex <landuser2020@gmail.com>
2025-08-05 15:39:00 +0300

    feat(HW_№2.py): added max_in_range() function

--- PD_11/Hometasks/Algs_and_Data_Structures/HW_№2/HW_№2.py
+++ PD_11/Hometasks/Algs_and_Data_Structures/HW_№2/HW_№2.py
@@ -1,4 +1,25 @@
 
 def check_type(value, type):
     if not isinstance(value, type):
-        raise TypeError(f"Неправильное входное значение")
+        raise TypeError(f"Некорректное входное значение!")
+
+# Задание № 1:
+
+def max_in_range(arr: list, start: int, end: int) -> str:
+
+    check_type(arr, list)
+    length = len(arr)
+    check_type(start, int)
+    check_type(end, int)
+    if start > length or end > length:
+        raise ValueError("Указаны некорректные пределы! Начальный или конечный индекс не может быть больше длины списка!")
+
+    rel_max_index = start
+    abs_max_index = 0
+    for i in range(start + 1, end + 1):
+        if arr[i] > arr[rel_max_index]:
+            rel_max_index = i
+            abs_max_index = i + start
+
+    return (f"Максимальный элемент между {start}-м и {end}-м элементом массива равен {arr[rel_max_index]}"
+            f"Абсолютный индекс максимального элемента - {abs_max_index}\nОтносительный индекс максимального элемента - {rel_max_index}")
