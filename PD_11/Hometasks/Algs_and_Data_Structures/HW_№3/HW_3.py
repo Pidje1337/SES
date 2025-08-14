114123d63131d65b0ac128608139e2ed0f9f1408
Nick Kryutchkex <landuser2020@gmail.com>
2025-08-14 15:26:01 +0300

    fear(HW_3): added recursive_sum() function

--- PD_11/Hometasks/Algs_and_Data_Structures/HW_№3/HW_3.py
+++ PD_11/Hometasks/Algs_and_Data_Structures/HW_№3/HW_3.py
@@ -2,3 +2,20 @@
 def check_type(value: any, req_type: any) -> None or ValueError:
     if not isinstance(value, req_type):
         raise ValueError(f"Ошибка: Некорректный тип входный данных.\nОжидалось: {req_type}\nПолучено: {type(value)}")
+
+
+
+# Задание № 3
+
+def recursive_sum(array: list[int or float]) -> int or float:
+
+    check_type(array, list)
+    for elem in array:
+        check_type(elem, int or float)
+
+    length = len(array)
+
+    if length == 0:
+        return 0
+
+    return array.pop() + recursive_sum(array)
