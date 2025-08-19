121965298a186917878bb1c17f06a38d0b829478
Nick Kryutchkex <landuser2020@gmail.com>
2025-08-19 23:55:04 +0300

    feat(HW_5): added __init__ for inner class Node in Stack

--- PD_11/Hometasks/Algs_and_Data_Structures/HW_5/Stack/stack.py
+++ PD_11/Hometasks/Algs_and_Data_Structures/HW_5/Stack/stack.py
@@ -6,3 +6,9 @@ class Stack:
         self.top = None
         self.size = 0
 
+    class Node:
+
+        def __init__(self, value, next = None):
+            self.value = value
+            self.next = next
+
