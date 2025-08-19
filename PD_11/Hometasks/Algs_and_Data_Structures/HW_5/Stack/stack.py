5715fc6a2edea5046b9e05356f174ea340594412
Nick Kryutchkex <landuser2020@gmail.com>
2025-08-19 23:57:46 +0300

    fix(HW_5): excluded Node from Stack

--- PD_11/Hometasks/Algs_and_Data_Structures/HW_5/Stack/stack.py
+++ PD_11/Hometasks/Algs_and_Data_Structures/HW_5/Stack/stack.py
@@ -1,14 +1,12 @@
+class Node:
 
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
 
 class Stack:
 
     def __init__(self):
         self.top = None
         self.size = 0
-
-    class Node:
-
-        def __init__(self, value, next = None):
-            self.value = value
-            self.next = next
 
