3404d5e25b9b799461881c65b81009129e2fbf44
Nick Kryutchkex <landuser2020@gmail.com>
2025-08-24 00:50:16 +0300

    feat(Data_Analysis:HW_1): added solution for exercise 2

--- PD_11/Hometasks/Data_Analysis/HW_1/HW_1.py
+++ PD_11/Hometasks/Data_Analysis/HW_1/HW_1.py
@@ -2,6 +2,12 @@ import numpy as np
 
 # Задание № 1:
 
-temps = np.array([15.2, 16.8, 14.5, 17.0, 16.1])
-print(temps.sum(), "\n", temps.mean(), "\n", temps.min(), "\n", temps.max())
+# temps = np.array([15.2, 16.8, 14.5, 17.0, 16.1])
+# print(temps.sum(), "\n", temps.mean(), "\n", temps.min(), "\n", temps.max())
 
+# Задание № 2:
+
+# h1 = np.array([45, 50, 47])
+# h2 = np.array([48, 46, 52])
+# print(np.sum([h1, h2], axis = 0))
+# print(np.prod([h1, h2], axis = 0))
