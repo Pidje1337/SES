e885a0231cd9c34b870e19125e5052285cfc9285
Nick Kryutchkex <landuser2020@gmail.com>
2025-08-26 00:57:18 +0300

    feat(HW_Data_Structures): added add_last() method for DoubleLinkedList class

--- PD_11/Hometasks/Algs_and_Data_Structures/HW_Data_Structures/DoubleLinkedList/doublelinkedlist.py
+++ PD_11/Hometasks/Algs_and_Data_Structures/HW_Data_Structures/DoubleLinkedList/doublelinkedlist.py
@@ -19,3 +19,13 @@ class DoubleLinkedList:
 
     def is_empty(self):
         return self.size == 0
+
+    def add_last(self, value):
+
+        if self.is_empty():
+            self.tail = value
+        else:
+            value.prev = self.head
+
+        self.head = value
+        self.size += 1
