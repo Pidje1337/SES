fab5b40cfeec33b63406a53dbafa8e6739a0b40d
Nick Kryutchkex <landuser2020@gmail.com>
2025-08-26 00:34:32 +0300

    feat(HW_Data_Structures): added init method for Node inner class

--- PD_11/Hometasks/Algs_and_Data_Structures/HW_Data_Structures/DoubleLinkedList/doublelinkedlist.py
+++ PD_11/Hometasks/Algs_and_Data_Structures/HW_Data_Structures/DoubleLinkedList/doublelinkedlist.py
@@ -6,3 +6,10 @@ class DoubleLinkedList:
         self.size = 0
         self.head = None
         self.tail = None
+
+    class Node:
+
+        def __init__(self, value, prev = None):
+            self.value = value
+            self.prev = prev
+            self.next = None
