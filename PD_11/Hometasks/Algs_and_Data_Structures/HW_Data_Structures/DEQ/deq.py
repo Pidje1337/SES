79c5edfcd4fa85385124b08236b279af7885b41a
Nick Kryutchkex <landuser2020@gmail.com>
2025-08-26 17:15:02 +0300

    temp_bad_coomit: deq.py

---
+++ PD_11/Hometasks/Algs_and_Data_Structures/HW_Data_Structures/DEQ/deq.py
@@ -0,0 +1,51 @@
+
+
+class DEQ:
+
+    def __init__(self):
+        self.size = 0
+        self.head = None
+        self.tail = None
+
+    class Node:
+
+        def __init__(self, value, next, prev):
+            self.value = value
+            self.next = next
+            self.prev = prev
+
+    def size(self):
+        return self.size
+
+    def is_empty(self):
+        return self.size == 0
+
+    def clear(self):
+        self.size = 0
+        self.head = None
+        self.tail = None
+
+    def peek(self):
+        return self.head
+
+    def peek_tail(self):
+        return self.tail
+
+    def enqueue(self, node):
+        node.next = self.tail
+        self.tail.prev = node
+        self.tail = node
+
+    def enqueue_head(self, node):
+        node.prev = self.head
+        self.head.next = node
+        self.head = node
+
+    def dequeue(self):
+        self.head = self.head.prev
+        self.head.next = None
+
+    def dequeue_tail(self):
+        self.tail = self.tail.next
+        self.tail.prev = None
+
