e2c261809a8be5d0887609a0aef8bcc8110afc63
Nick Kryutchkex <landuser2020@gmail.com>
2025-11-21 19:13:54 +0300

    refactor(gui.py:launch_mock): edited logic of launch_mock()

--- Note_manager/Project_note_manager/gui.py
+++ Note_manager/Project_note_manager/gui.py
@@ -3,11 +3,12 @@ from time import sleep
 
 def launch_mock() -> None:
     rand_sum = 0
-    while rand_sum <= 100:
-        print("\rИдёт подготовка программы...", end="   [")
-        print("\u25A0" * (rand_sum // 10), end="")
-        print("." * (10 - rand_sum // 10), end="]")
-        print(f"{rand_sum} %")
+    while rand_sum < 100:
+        output = "\rИдёт подготовка программы...   ["
+        output += "\u25A0" * (rand_sum // 10)
+        output += "." * (10 - rand_sum // 10) + "]"
+        output += f"{rand_sum}%"
+        print(output)
         rand_sum += randint(6, 14)
         sleep(uniform(0.4, 1.3))
     print(f"\r[" + "\u25A0" * 10 + f"]   {100}%\nПрограмма готова к работе!")
