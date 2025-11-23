d795c5baf9e14abba9b5b242ea6c8f41dc375851
Nick Kryutchkex <landuser2020@gmail.com>
2025-11-23 20:18:39 +0300

    add(bl_upper.py): added "create_user()" function

--- Note_manager/Project_note_manager/bl_upper.py
+++ Note_manager/Project_note_manager/bl_upper.py
@@ -3,6 +3,16 @@ from datetime import date
 from pathlib import Path
 
 
+# Функция создания пользователя
 
-
-
+def create_user():
+    app_directory = Path(Path.home()/'note_manager_by_Kryutchkex')
+    username = input("Введите имя пользователя, которого хотите создать: ")
+    while not username_validation(username):
+        username = input("Введённое имя пользователя содержит апрещённые символы или ошибки наименования"
+                         "\nПожалуйста, введите другое имя пользователя, которого хотите создать: ")
+    while does_user_exist(username):
+        username = input("Данный пользователь уже существует!\nПожалуйста, введите другое имя пользователя, которого хотите создать: ")
+    file = app_directory/f"{username}.txt"
+    file.touch()
+    file.write_text("Наименование, Тип заметки, Приоритет, Дата создания, Содержание")
